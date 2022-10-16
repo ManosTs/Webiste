b48497f0d341c2dd46492f4bb0bf0baa8bd0f8ff
ManosTs <mtsiorbas@gmail.com>
2022-10-16 18:44:35 +0300

    auth

--- src/main/java/com/project/website/controller/UserController.java
+++ src/main/java/com/project/website/controller/UserController.java
@@ -1,8 +1,54 @@
 package com.project.website.controller;
 
 
-import org.springframework.web.bind.annotation.RestController;
+import com.project.website.entity.User;
+import com.project.website.repository.UserRepository;
+import com.project.website.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/user")
 public class UserController {
+    private UserRepository userRepository;
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path= "/register", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> registerUser(@RequestBody User user) {
+        User userFound = userRepository.findByEmail(user.getEmail());
+
+        if(userFound != null){
+            return ResponseEntity.status(HttpStatus.FOUND).body("EMAIL ALREADY EXISTS"); //user exists
+
+        }
+
+        User newUser = userService.createUser(user.getEmail(), user.getUsername(), user.getPassword());
+
+        return ResponseEntity.status(HttpStatus.OK).body(newUser);
+    }
+
+    @PostMapping(path= "/login", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Object> loginUser(User user) {
+        User userFound = userService.authUser(user.getEmail(), user.getPassword());
+
+        if(userFound != null){
+            return ResponseEntity.status(HttpStatus.OK).body(userFound);
+
+        }
+
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+    }
 }
--- src/main/java/com/project/website/service/UserService.java
+++ src/main/java/com/project/website/service/UserService.java
@@ -1,7 +1,50 @@
 package com.project.website.service;
 
+import com.project.website.entity.User;
+import com.project.website.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    public void setbCryptPasswordEncoder(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Autowired
+    public void setUserRepository(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User createUser(String email, String username, String password){
+        User newUser = new User();
+
+        String encodedPassword = bCryptPasswordEncoder.encode(password);
+
+        newUser.setEmail(email);
+        newUser.setUsername(username);
+        newUser.setPassword(encodedPassword);
+
+        userRepository.save(newUser);
+
+        return newUser;
+    }
+
+    public User authUser(String email, String password){
+        User userExists = userRepository.findByEmail(email);
+
+        if(userExists != null && bCryptPasswordEncoder.matches(password, userExists.getPassword())){
+            return userExists;
+        }
+
+        return null;
+    }
 }
