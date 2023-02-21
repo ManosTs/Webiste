a03fc0982cf25430fb3f165e025943557d242b02
Emmanouil Tsiorvas <etsiorvas@deloitte.gr>
2023-02-21 17:18:50 +0200

    css prof fix

--- src/main/react-ui/src/components/Profile.js
+++ src/main/react-ui/src/components/Profile.js
@@ -21,25 +21,34 @@ function Profile(){
     };
 
     return(
-        <div className="profile--wrapper">
-            <div className="profile--wrapper__media">
-                <img alt="" className="profile--wrapper__image"></img>
-                <img alt="" className="profile--wrapper__backgroundImage"></img>
-            </div>
-            <div className="profile--wrapper__info">
-                <div className="profile--wrapper__username">{loggedUser.username}</div>
-                <div className="profile--wrapper__email">{loggedUser.email}</div>
-            </div>
+        <div className="profile-grid">
+            <div className="profile--settings">
 
-            <div className="profile--wrapper__tabs">
-                <ul>
-                    <li><NavLink to={'tab1'}>TAB 1</NavLink></li>
-                    <li><NavLink to={'tab2'}>TAB 2</NavLink></li>
-                    <li><NavLink to={'tab3'}>TAB 3</NavLink></li>
-                </ul>
             </div>
-            <Outlet />
+            <div className="profile--wrapper">
+                <div className="profile--wrapper__media">
+                    <img alt="" className="profile--wrapper__image"></img>
+                    <img alt="" className="profile--wrapper__backgroundImage"></img>
+                </div>
+                <div className="profile--wrapper__info">
+                    <div className="profile--wrapper__username">{loggedUser.username}</div>
+                    <div className="profile--wrapper__email">{loggedUser.email}</div>
+                </div>
+
+                <div className="profile--wrapper__tabs">
+                    <ul>
+                        <li><NavLink to={'tab1'}>TAB 1</NavLink></li>
+                        <li><NavLink to={'tab2'}>TAB 2</NavLink></li>
+                        <li><NavLink to={'tab3'}>TAB 3</NavLink></li>
+                    </ul>
+                </div>
+                <Outlet />
+            </div>
+            <div className="profile--settings">
+
+            </div>
         </div>
+
     );
 }
 
